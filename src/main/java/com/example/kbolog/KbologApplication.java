57c82aa82ea546af36c43f3e3f80ae75991944a9
유경 <eeheueklf@gmail.com>
2024-11-18 23:34:39 +0900

    first setting

---
+++ src/main/java/com/example/kbolog/KbologApplication.java
@@ -0,0 +1,13 @@
+package com.example.kbolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbologApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kbolog/KbologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kbolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
